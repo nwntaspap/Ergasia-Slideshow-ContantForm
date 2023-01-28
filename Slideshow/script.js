3dff27d64eec2b69a2f7cd0ea6eb785af10e02a6
sotirispinakoulas <sotirispinakoulas@gmail.com>
2023-01-28 21:56:28 +0200

    WIP slideshow implementation

--- Slideshow/script.js
+++ Slideshow/script.js
@@ -1,44 +1,59 @@
-// Select all slides
-const slides = document.querySelectorAll(".slide");
-// select next slide button
-const nextSlide = document.querySelector(".next");
-// select prev slide button
-const prevSlide = document.querySelector(".prev")
-// current slide counter
-let currentSlide = 0;
-// maximum number of slides
-let maxSlide = slides.length - 1;
+window.addEventListener('DOMContentLoaded', () => {
+    const images = [
+        'slide1.png',
+        'slide2.jpg',
+        'slide3.png',
+        'slide4.png',
+        'slide5.jpg',
+    ];
+    let currentImageIndex = 0;
 
-// loop through slides and set each slides translateX property to index * 100% 
-slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${indx * 100}%)`;
-  });
+    const previousBtn = document.querySelector("#previousBtn");
+    const nextBtn = document.querySelector("#nextBtn");
+    const thubnailsContainer = document.querySelector('#thubnailsContainer');
 
-// add event listener and navigation functionality
-nextSlide.addEventListener("click", function () {
-    // check if current slide is the last and reset current slide
-    if (currentSlide === maxSlide) {
-        currentSlide = 0;
-    } else {
-        currentSlide++;
-    }
-  
-  //   move slide by -100%
-    slides.forEach((slide, indx) => {
-      slide.style.transform = `translateX(${100 * (indx - currentSlide)}%)`;
-    });
-  });
+    const print = () => {
+         const currentImage = images[currentImageIndex];
+         const slideImage = document.querySelector('#carousel img.slide');
+         const thubnailsContainer = document.querySelector('#thubnailsContainer');
 
-prevSlide.addEventListener("click", function () {
-    // check if current slide is the last and reset current slide
-    if (currentSlide === 0) {
-        currentSlide = maxSlide;
-    } else {
-        currentSlide--;
+         slideImage.src = './images/' + currentImage;
+
+         let thubnailsImagesTemplate = '';
+         images.forEach((image, index) => {
+             let classNames = 'dot';
+             if(index === currentImageIndex) {
+                 classNames += ' active'
+             }
+             thubnailsImagesTemplate += `
+                <img class="${classNames}" 
+                     src="./images/${image}" 
+                />
+            `;
+         });
+         thubnailsContainer.innerHTML = thubnailsImagesTemplate;
     }
 
-    //   move slide by 100%
-    slides.forEach((slide, indx) => {
-        slide.style.transform = `translateX(${100 * (indx - currentSlide)}%)`
-    })
-});
+    const previewNextImage = () => {
+        if(currentImageIndex === images.length -1) {
+            currentImageIndex = 0;
+        } else {
+            currentImageIndex++;
+        }
+        print();
+    }
+    const previewPreviousImage = () => {
+        if(currentImageIndex === 0) {
+            currentImageIndex = images.length - 1;
+        } else {
+            currentImageIndex--;
+        }
+        print();
+    }
+
+    nextBtn.addEventListener('click', previewNextImage);
+
+    previousBtn.addEventListener('click', previewPreviousImage);
+
+    print();
+})
