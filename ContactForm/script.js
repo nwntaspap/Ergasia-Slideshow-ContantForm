fa3afaf2617b58d07b7eb19cb0747b70b44441eb
sotirispinakoulas <sotirispinakoulas@gmail.com>
2023-01-29 18:35:30 +0200

    complete contact form implementation

--- ContactForm/script.js
+++ ContactForm/script.js
@@ -4,16 +4,66 @@ document.addEventListener('DOMContentLoaded', () => {
 	formEl.addEventListener('submit', e => {
 		e.preventDefault();
 		const formData = new FormData(formEl);
-		const errors = {};
+		const nameError = document.querySelector('#nameError');
+		const emailError = document.querySelector('#emailError');
+		const commentsError = document.querySelector('#commentsError');
+		const phoneError = document.querySelector('#phoneError');
+		const successMessage = document.querySelector('#successMessage');
 
+		//clear old errors (reset)
+		nameError.innerHTML = '';
+		emailError.innerHTML = '';
+		commentsError.innerHTML = '';
+		phoneError.innerHTML = '';
+
+		let errors = [];
 		//Validate name
 		if(formData.get('name').trim().length === 0) {
-			errors['name'] = 'Please fill the Name'
+			nameError.innerHTML = 'Name is required';
+			errors.push('name');
 		}
 
 		//Validate email
 		if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData.get('email'))) {
-			alert('no valid email')
+			emailError.innerHTML = 'Please enter a valid email!'
+			errors.push('email');
+		}
+
+		//Validate comments
+		if(formData.get('comments').trim().length === 0) {
+			commentsError.innerHTML = 'Comments are required!!'
+			errors.push('comments');
+		} else if (formData.get('comments').trim().length < 10) {
+			commentsError.innerHTML = 'Comments should have at least 10 characters but you have inserted: '
+				+ formData.get('comments').length;
+			errors.push('comments');
+		}
+
+		//Validate phone
+		const phoneNumber = formData.get('phone').trim();
+		if(isNaN(phoneNumber)) {
+			phoneError.innerHTML = 'Phone number should be a number!'
+			errors.push('phone');
+		} else {
+			if(phoneNumber.length !== 10) {
+				phoneError.innerHTML = 'Phone number must have 10 digits!'
+				errors.push('phone');
+			}
+		}
+
+
+		if(errors.length === 0) {
+			let message = '';
+			message += `Your name is: ${formData.get('name').trim()} </br>`;
+			message += `Your email is: ${formData.get('email').trim()} </br>`;
+			message += `Your comments are: ${formData.get('comments').trim()} </br>`;
+			message += `Your phone is: ${formData.get('phone').trim()} </br>`;
+
+			successMessage.innerHTML = message;
+			successMessage.style.display = 'block';
+			formEl.reset();
+		} else {
+			successMessage.style.display = 'none';
 		}
 	})
 })
